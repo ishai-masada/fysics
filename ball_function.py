fc9cedbb125f89842b9846dd50ee5d128128017b
Ishai 'Doofus' Masada <imasada@vtcap.org>
2021-05-21 16:41:00 -0400

    simulated function

--- ball_function.py
+++ ball_function.py
@@ -8,7 +8,7 @@ class Vector2:
 def ball_throw(ang, speed):
     """ angle is in degrees and speed in meters/second"""
     
-    fysics_dict = dict()
+    ball_throw_dict = dict()
     # Convert angle to radians
     converted_ang = (ang * math.pi) / 180
 
@@ -17,7 +17,7 @@ def ball_throw(ang, speed):
     
     acceleration = 9.8
     
-    # calculate the time it takes for ball to land
+    # Calculate the time it takes for ball to land
     time = round(2 * v_speed/acceleration, 2)
 
     # Check to see if trig functuion made time negative
@@ -27,25 +27,54 @@ def ball_throw(ang, speed):
     # Use the range equation to find the distance
     distance = round((speed**2 * math.sin(2*converted_ang))/ acceleration, 2)
 
-    # Put time and distance into dictionary
-    fysics_dict["distance"] = distance
-    fysics_dict["time"] = time
+    # Store time and distance into dictionary
+    ball_throw_dict["distance"] = distance
+    ball_throw_dict["time"] = time
 
     return fysics_dict
 
 
 def simulated_ball_throw(ang, speed):
-    steps_per_sec = 100
+    acceleration = 9.8
+    simulated_dict = dict()
+    steps_per_sec = 500
     position = Vector2()
     converted_ang = (ang * math.pi) / 180
     v_speed = speed * math.sin(converted_ang)
     h_speed = speed * math.cos(converted_ang)
     velocity = Vector2(v_speed, h_speed)
+ 
     
     while True:
         position.x += velocity.x/steps_per_sec
         position.y += velocity.y/steps_per_sec
         velocity.y -= 9.8/steps_per_sec
+        distance = abs(round(position.x, 1))
+        
+        
+        # Calculate the time it takes for ball to land
+        time = round(2 * v_speed/acceleration, 2)
+
+        # Check to see if trig functuion made time negative
+        if time < 0:
+            time = -1 * time
+
+        # Verify the velocity to find the maximum height
+        if velocity.y >= 0:
+            max_altitude = round(position.y, 2)
+            
+            # Store the maximum altitude into the dictionary
+            simulated_dict["maximum height"] = max_altitude
+
+            # Find the magnitude of the vector
+            hyp_vel = (h_speed**2 + v_speed**2)**.5
+
+            #Store the max velocity into the dictionary
+            simulated_dict["maximum velocity"] = round(hyp_vel, 2)
+
+        # Store time and range into dictionary
         if position.y < 0:
-            return abs(position.x)
+            simulated_dict["distance"] = distance
+            simulated_dict["time"] = time
+            return simulated_dict
     
